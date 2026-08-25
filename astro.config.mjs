fed6be50ba6b5fbd7f08b2282bd487bd623e310d
kodepl <koode9@gmail.com>
2026-08-25 13:04:43 +0200

    Add astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import rehypeSlug from "rehype-slug";
+
+export default defineConfig({
+  site: "https://www.genetyka.com.pl",
+  trailingSlash: "always",
+  compressHTML: true,
+  markdown: {
+    rehypePlugins: [rehypeSlug],
+  },
+  integrations: [tailwind()],
+});
